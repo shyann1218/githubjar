a6916fb16fe4dc04d0941ae0d932e9d1042033e9
shyann1218 <31852373+shyann1218@users.noreply.github.com>
2021-04-08 17:40:40 +0800

    Update TestClass.java
    modify testclass

--- jarlib/src/main/java/com/yaxon/jarlib/TestClass.java
+++ jarlib/src/main/java/com/yaxon/jarlib/TestClass.java
@@ -3,5 +3,15 @@ package com.yaxon.jarlib;
 import com.yaxon.mylibrary.bean.LoginProtocol;
 
 public class TestClass {
+    /**
+     * 登录
+     *
+     * @param username  用户名
+     * @param password  密码
+     */
+    public static Observable<LoginBean> login( String username, String password) {
 
+           return LoginProtocol.getInstance().login(username, password);
+
+    }
 }
